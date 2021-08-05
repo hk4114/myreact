27fb80147ddfca952e444e657df1cc3486ba41e2
kz-fe <741180788@qq.com>
2021-08-05 23:10:44 +0800

    create element

--- src/index.js
+++ src/index.js
@@ -1,34 +1,9 @@
 import React from './core'
 
-class Demo extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      count: 1
-    }
-  }
-  handleClick = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-  render() {
-    return <div>
-      <h2 onClick={this.handleClick}>{this.state.count}</h2>
-    </div>
-  }
-}
-Demo = React.transfer(Demo)
-
-function App(props) {
-  const [count, setCount] = React.useState(1)
-  return <div id="container" className="red">
-    <h1>{props.title}, {count}</h1>
-    <button onClick={() => setCount(count + 1)}>??</button>
-    <hr />
-    <Demo></Demo>
-  </div>
-}
-let element = <App title="huakang" />
+const element = <div>
+  <h1>demo</h1>
+  <p>for sample <b>react</b></p>
+  <a href="https://github.com/hk4114/myreact">repo</a>
+</div>
 
 React.render(element, document.getElementById('root'))
