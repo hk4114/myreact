72c2ae96acfab1a9337a38b28d46e32660a705fe
kz-fe <741180788@qq.com>
2021-10-01 00:39:52 +0800

    update: 可中断的渲染 requestIdleCallback 和 fiber

--- src/core/index.js
+++ src/core/index.js
@@ -1,3 +1,56 @@
+let nextUnitOfWork = null;
+
+function workloop(deadline) {
+  while (nextUnitOfWork && deadline.timeRemaining() >= 1) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+  }
+  requestIdleCallback(workloop)
+}
+requestIdleCallback(workloop)
+
+function performUnitOfWork(fiber) {
+  if(!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  if(fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+  
+  const elements = fiber.props.children;
+  let prevSibling = null;
+  for(let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null
+    }
+
+    if(i===0) {
+      fiber.child = newFiber
+    }else if(element) {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber
+  }
+
+  if(fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber;
+  while(nextFiber) {
+    if(nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+
+
 function createElement(type, props, ...children) {
   delete props.__source
   return {
@@ -19,17 +72,23 @@ function craeteTextElement(nodeValue) {
   }
 }
 
-function render(vdom, container) {
+function createDom(vdom) {
   const dom = vdom.type === 'TEXT' ? document.createTextNode('') : document.createElement(vdom.type);
   Object.keys(vdom.props).forEach(prop => {
     if (prop !== 'children') {
       dom[prop] = vdom.props[prop]
     }
   })
-  vdom.props.children.forEach(child => {
-    render(child, dom)
-  })
-  container.appendChild(dom)
+  return dom
+}
+
+function render(vdom, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [vdom]
+    }
+  }
 }
 
 export default {
